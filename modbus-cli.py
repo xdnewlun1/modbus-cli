d3874c37657229a7d8bb8618d440988c8731ed3b
xdnewlun1 <Xander.Newlun@my.utsa.edu>
2024-12-07 23:45:32 -0600

    Added Comments and Input Validation
    Added comments to code
    Added validation to ensure numbers are able to be parsed.

--- modbus-cli.py
+++ modbus-cli.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python3.11
 import argparse
 from pymodbus.client import ModbusTcpClient
 from pymodbus.payload import BinaryPayloadDecoder
 from pymodbus.constants import Endian
 import struct
 
-#Get the coil value at the specified address | Givel location %QX0.0-0.4 coil_address should be %QX0.coil_address
+#Get the coil value at the specified address | Given location %QX0.0-0.4 coil_address should be %QX0.coil_address
 def get_coil(client, coil_address):
 	return client.read_coils(coil_address, slave=1).bits[0]
 
@@ -91,91 +90,131 @@ def return_bool_val(value):
 #arg parser
 parser = argparse.ArgumentParser(description="Modbus Interaction from the CLI to Read/Write to Coils and Registers")
 
+#Add the read and write options, and allow user to only choose one.
 group = parser.add_mutually_exclusive_group(required=True)
 group.add_argument("-r", "--read", action="store_true", help="Read from the given modbus address")
 group.add_argument("-w", "--write", action="store_true", help="Write to a given modbus address")
 
+#Add the IP and Port options. The IP is required, but port has a default value and is not
 parser.add_argument("--ip", action="store", help="The IP address of the ModBus Device", required=True)
 parser.add_argument("--port", action="store", default=502, help="The port of the the ModBus Service", required=False)
 
+#Add the address value. This is required to identify the correct register/coil
 parser.add_argument("-a", "--address", action="store", help="Target address on the ModBus Device")
 
+#Add the register types, these are required but exclusive to not confuse the program.
 types = parser.add_mutually_exclusive_group(required=True)
 types.add_argument("-i", "--integer", action="store_true", help="Select a 16bit Integer as the target at the address")
 types.add_argument("-f", "--float", action="store_true", help="Select a 32bit Float as the target at the address")
 types.add_argument("-c", "--coil", action="store_true", help="Select a coil as the target at the address")
 
+#Add the value option, this is only necessary if doing a write operation.
 parser.add_argument("-v", "--value", action="store", help="The new value to store at the given address")
 
+#Get the args
 args = parser.parse_args()
 
+#Check if the value arg is set when trying to write
 if args.write and args.value is None:
 	parser.error("-w requires -v arguement")
 
+#Check that the value argument is not set when trying to read
 if args.read and args.value is not None:
 	parser.error("-r does not accept the -v argument")
 
-if args.write and args.coil and int(args.value) not in [0, 1]:
+#Check that the value is only 0, or 1 when writing to a coil.
+#This check is being done with the value still as a string to mitigate a ValueError if I int the value and its not valid.
+if args.write and args.coil and args.value not in ["0", "1"]:
 	parser.error("-c requires -v is only 1 (true) or 0 (false)")
 
+#If writing and the target is a float, then confirm that the given value can be converted to a float
+if args.write and args.float:
+	try:
+		float(args.value)
+	except ValueError:
+		parser.error("-v value is not a valid float")
+
+#If writing and the target is an integer, then confirm that the given value can be converted to an int
+if args.write and args.integer:
+	try:
+		int(args.value)
+	except ValueError:
+		parser.error("-v value is not a valid integer")
+
 #Connect to Modbus TCP Server on PLC
-client = ModbusTcpClient(f"{args.ip}")
+client = ModbusTcpClient(f"{args.ip}",port=int(args.port))
 client.connect()
 
-print(args)
-
+#Logic for is -c is selected
 if args.coil:
+	#Check if reading
 	if args.read:
+		#Do the read operation
 		operation = get_coil(client, int(args.address))
+		#Check the read was successful, if not then report to user the failure
 		if operation != None:
 			print(f"Success: {args.ip}:{args.port} Coil {args.address} = {operation}")
 		else:
 			print(f"Error: Unable to Read Value from coil {args.address}")
+
+	#Check if writing
 	if args.write:
+		#Get the original value before doing the set
 		old_val = get_coil(client, int(args.address))
+		#Complete the write operation
 		operation = set_coil(client, int(args.address), int(args.value))
+		#Check if the operating was successful if not alert the user to the error.
 		if operation:
 			print(f"Success: {args.ip}:{args.port} Coil {args.address} changed from {old_val} to {return_bool_val(int(args.value))}")
 		else:
 			print(f"Error: Unable to set value")
 
+
+#Logic for if -f is selection
 if args.float:
+	#Check if reading
 	if args.read:
+		#Do the read operation
 		operation = get_32bit_register(client, int(args.address))
+		#Confirm the read was successful, and output the results. If the read failed then alert the user
 		if operation != None:
 			print(f"Success: {args.ip}:{args.port} Register {args.address}-{int(args.address)+1} = {operation}")
 		else:
 			print(f"Error: Reading {args.ip}:{args.port} Register {args.address}")
+
+	#Check if writing		
 	if args.write:
+		#Get the original value before doing the write
 		old_val = get_32bit_register(client, int(args.address))
+		#Complete the write operation
 		operation = set_32bit_register(client, int(args.address), float(args.value))
+		#If successful let the user know of the change, if unsuccessful then alert them
 		if operation:
 			print(f"Success: {args.ip}:{args.port} Register {args.address}-{int(args.address)+1} changed from {old_val} to {args.value}")
 		else:
 			print(f"Error: Unable to set value")
 
+
+#Logic for if -i is selection
 if args.integer:
+	#Check if reading
 	if args.read:
+		#Do the read operation
 		operation = get_16bit_register(client, int(args.address))
+		#If the read was successful then let the user know, otherwise throw an error
 		if operation != None:
 			print(f"{args.ip}:{args.port} Register {args.address} = {operation}")
 		else:
 			print(f"Error: Unable to Read {args.ip}:{args.port} Register {args.address}")
+
+	#Check if writing
 	if args.write:
+		#Get the original value before doing the write
 		old_val = get_16bit_register(client, int(args.address))
+		#Complete the write operation
 		operation = set_16bit_register(client, int(args.address), int(args.value))
+		#If successful let the user know of the change, fi unsuccessful then alert them.
 		if operation:
 			print(f"Success: {args.ip}:{args.port} Register {args.address} changed from {old_val} to {args.value}")
 		else:
 			print(f"Error: Unable to set value")
-
-
-
-
-#Arguments
-# -r -- READ (exclusive from -w) *
-# -w VALUE -- WRITE (exclusive from -r) *
-# --ip -- PLC IP-Address *
-# --port -- PLC Port (default modbus standard port)
-# --type -- Coil, Register (16bit int), Register (32bit Float) *
-# -a ADDRESS, Address for coil/register to read
